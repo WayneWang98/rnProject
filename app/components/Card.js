cb461abc545be1480fb80f1068dc9b3c29e8ae4f
waynewang98 <1070546617@qq.com>
2021-08-15 20:38:58 +0800

    realize the check style of bar

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   Image,
-  Button,
-  Alert,
   Animated,
   Easing,
+  ImageBackground,
 } from 'react-native';
 
 const myList = [
@@ -16,42 +15,56 @@ const myList = [
     score: 21,
     desc: '六',
     level: 'middle',
+    isSelected: false,
+    isToday: false,
   },
   {
     id: 2,
     score: 0,
     desc: '日',
     level: 'unknown',
+    isSelected: false,
+    isToday: false,
   },
   {
     id: 3,
     score: 81,
     desc: '一',
     level: 'middle',
+    isSelected: false,
+    isToday: false,
   },
   {
     id: 4,
     score: 90,
     desc: '二',
     level: 'high',
+    isSelected: false,
+    isToday: false,
   },
   {
     id: 5,
     score: 61,
     desc: '三',
     level: 'middle',
+    isSelected: false,
+    isToday: false,
   },
   {
     id: 6,
     score: 96,
     desc: '四',
     level: 'high',
+    isSelected: false,
+    isToday: false,
   },
   {
     id: 7,
     score: 99,
     desc: '五',
     level: 'high',
+    isSelected: false,
+    isToday: true,
   },
 ];
 
@@ -68,18 +81,61 @@ const moodColor = {
 };
 
 const Card = () => {
-  myList.forEach(item => {
-    item.barHeight = new Animated.Value(0);
-    const barGrowAnimated = Animated.timing(item.barHeight, {
-      toValue: item.score ? item.score * 1.75 + 40 : 80,
-      duration: 1500,
-      // easing: Easing.linear,
-    });
+  const [dataList, setDataList] = useState(myList);
+  const [hasCalled, setHasCalled] = useState(false);
+  const playAnimation = () => {
+    if (hasCalled) return;
+    setHasCalled(true);
+    dataList.forEach((item, index) => {
+      item.barHeight = new Animated.Value(0);
+      const barGrowAnimated = Animated.timing(item.barHeight, {
+        toValue: item.score ? item.score * 1.5 + 40 : 50,
+        duration: 1000,
+        easing: Easing.linear,
+        delay: 100 * index,
+      });
+      item.textOpacity = new Animated.Value(0);
+      const barTextOpacityAnimated = Animated.timing(item.textOpacity, {
+        toValue: 1,
+        duration: 600,
+        easing: Easing.linear,
+        delay: 100 * index,
+        useNativeDriver: true,
+      });
+      item.moodIcon = new Animated.Value(0);
+      const moodIconAnimated = Animated.timing(item.moodIcon, {
+        toValue: 1,
+        duration: 600,
+        easing: Easing.linear,
+        delay: 100 * index,
+        useNativeDriver: true,
+      });
 
-    barGrowAnimated.start(() => {
-      return item.barHeight.setValue(item.score ? item.score * 1.75 + 40 : 80);
+      item.barGrowAnimated = barGrowAnimated;
+      item.barTextOpacityAnimated = barTextOpacityAnimated;
+      item.moodIconAnimated = moodIconAnimated;
+      // item.backgroundAnimated = backgroundAnimated;
     });
-  });
+    dataList.forEach(item => {
+      const {barGrowAnimated, barTextOpacityAnimated, moodIconAnimated} = item;
+      barGrowAnimated.start(() => {
+        return item.barHeight.setValue(item.score ? item.score * 1.5 + 40 : 50);
+      });
+      barTextOpacityAnimated.start();
+      moodIconAnimated.start();
+    });
+  };
+
+  playAnimation();
+
+  const changeSelected = item => {
+    if (item.isSelected) return;
+    // dataList.forEach(data => {
+    //   data.isSelected = false;
+    // });
+    item.isSelected = true;
+    setDataList([...dataList]);
+  };
 
   return (
     <View style={styles.card}>
@@ -95,23 +151,47 @@ const Card = () => {
         <Text style={styles.desc}>周平均心情指数</Text>
       </View>
       <View style={styles.statistic}>
-        {myList.map(item => {
+        {dataList.map(item => {
+          const dateTextColor = item.isToday
+            ? '#fff'
+            : item.isSelected
+            ? moodColor[item.level]
+            : '#111';
+          const dateTextBgColor = item.isToday ? '#111' : '#fff';
           return (
             <View style={styles.statisticItem} key={item.id}>
               <Animated.View
                 style={{
                   ...styles.bar,
-                  // height: item.score ? item.score * 1.75 + 40 : 80,
                   height: item.barHeight,
                   backgroundColor: moodColor[item.level],
                 }}>
-                <Text style={styles.barScore}>{item.score}</Text>
-                <Image
-                  style={styles.barImg}
+                <Text style={styles.barScore}>{item.score ? item.score : ''}</Text>
+                <Animated.Image
+                  style={{
+                    ...styles.barImg,
+                    transform: [
+                      {
+                        scale: item.moodIcon,
+                      },
+                    ],
+                  }}
                   source={moodLevelIcon[item.level]}
                 />
               </Animated.View>
-              <Text style={styles.date}>{item.desc}</Text>
+              <Animated.Text
+                style={{
+                  ...styles.date,
+                  opacity: item.textOpacity,
+                  color: dateTextColor,
+                  backgroundColor: dateTextBgColor,
+                  elevation: item.isSelected ? 6 : 0,
+                }}
+                onPress={() => {
+                  changeSelected(item);
+                }}>
+                {item.desc}
+              </Animated.Text>
             </View>
           );
         })}
@@ -167,12 +247,14 @@ const styles = StyleSheet.create({
     marginLeft: 12,
   },
   bar: {
-    // backgroundColor: '#52C873',
+    position: 'relative',
     borderRadius: 30,
     alignItems: 'center',
     justifyContent: 'space-between',
   },
   barImg: {
+    position: 'absolute',
+    bottom: 0,
     width: 32,
     height: 36,
     resizeMode: 'contain',
@@ -185,6 +267,8 @@ const styles = StyleSheet.create({
   date: {
     textAlign: 'center',
     marginTop: 8,
+    lineHeight: 36,
+    borderRadius: 8,
   },
 });
 
